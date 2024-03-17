f406c93159c3bbb97964ace5c25db5e1e9bdfac4
Parth Rasal <parth.rasal@yahoo.com>
2024-03-17 22:33:22 +0530

    Add background for Login screen

---
+++ assgn/src/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.css'; // Import CSS file for styling
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      Hi
+    </div>
+  );
+};
+
+export default Home;
